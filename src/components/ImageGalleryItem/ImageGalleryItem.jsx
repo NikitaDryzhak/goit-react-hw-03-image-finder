f1ac72fadfdbab8d1c71223e845463f19c70a7b2
Nikita Dryzhak <87204570+NikitaDryzhak@users.noreply.github.com>
2022-06-30 21:48:59 +0300

    done

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,25 @@
+import s from './ImageGalleryItem.module.css';
+import PropTypes from 'prop-types';
+
+const ImageGalleryItem = ({ id, webformatURL, largeImageURL, openModal }) => {
+  return (
+    <li className={s.galleryItem} key={id}>
+      <img
+        src={webformatURL}
+        srcSet={largeImageURL}
+        alt="picture"
+        className={s.galleryImg}
+        onClick={openModal}
+      />
+    </li>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  openModal: PropTypes.func.isRequired,
+};
+
+export default ImageGalleryItem;
